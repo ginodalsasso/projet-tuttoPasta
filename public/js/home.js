d17c70e053c6a48fa8442ef7b6b56e3133fcc754
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-07-30 20:04:08 +0200

    section service home nearly done

--- public/js/home.js
+++ public/js/home.js
@@ -2,11 +2,19 @@
 $(document).ready(function() {
     // Variable de couleur pour les H2 des cards articles
     var colors = ['var(--pink-color)', 'var(--red-color)', 'var(--blue-color)', 'var(--green-color)'];
+    // Couleur aléatoire pour chaque élément de la classe .badges_guide i
     $(".badges_guide i").each(function(index) {
         $(this).css('color', colors[index % colors.length]);
     });
 
-    $(".stickers_price").each(function() {
-        $(this).css('color', colors[index % colors.length]);
+    var stickerClasses = ['stickers_pink', 'stickers_red', 'stickers_blue', 'stickers_green'];
+    // Couleur aléatoire pour chaque élément de la classe service_cards_header
+    $(".service_cards_header").each(function(index) {
+        var color = colors[index % colors.length];
+        var stickerClass = stickerClasses[index % stickerClasses.length];
+        
+        $(this).css('color', color); // Change la couleur du H2
+
+        $(this).find('.stickers_price').addClass(stickerClass); // Ajouter la classe de sticker à stickers_price
     });
 });
--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -6,9 +6,10 @@ use App\Entity\Category;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 #[IsGranted('ROLE_ADMIN')]
 class CategoryCrudController extends AbstractCrudController
@@ -22,12 +23,16 @@ class CategoryCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('categoryName'),
-            TextEditorField::new('categoryContent'),
-            CollectionField::new('projects'),
-            CollectionField::new('articles'),
-            CollectionField::new('services'),
+            TextEditorField::new('categoryContent')
+            ->setTrixEditorConfig([
+                'blockAttributes' => [
+                    'default' => ['tagName' => 'p'],
+                ],
+            ]),
+            AssociationField::new('projects'),
+            AssociationField::new('articles'),
+            AssociationField::new('services'),
         ];
     }
 }
